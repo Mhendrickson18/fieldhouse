3dbff15b745250c315f118ac29723ae32db63419
Marshall Hendrickson <marshall@skylinefootball.net>
2014-03-21 12:01:42 -0600

    Created a rails application and added status updates

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Fieldhouse::Application.config.secret_key_base = 'c85184c798e5e3a1c88853420a34d65adff18b1bd8530b9290eed75351ea910673dde3c808f9b0dcefc0d48e39d4e085e424ecfa5a5bd3c9f76bd56e8227a94a'
